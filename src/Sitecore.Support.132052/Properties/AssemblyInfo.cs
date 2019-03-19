c1da9d961669d11b0ccd1b14d44560576ab3e03e
Vladyslav Molibozhenko <vlm@sitecore.net>
2019-03-19 13:15:18 +0200

    Patch Commit

---
+++ src/Sitecore.Support.132052/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.132052")]
+[assembly: AssemblyProduct("Sitecore.Support.132052")]
+[assembly: ComVisible(false)]
